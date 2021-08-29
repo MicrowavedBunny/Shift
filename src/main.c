72770ba5188276da6ed868ce85210128da91ca61
Jacob Davidson <jayz34567@gmail.com>
2021-08-29 07:50:44 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,33 +1,96 @@
 #include "raylib.h"
-
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
-
+#include <stdio.h>
 int main(void)
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
-    SetTargetFPS(60);
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png");
+    InitWindow(screenWidth, screenHeight, "Shift");
+    
+    Rectangle mainSqaure = { 535.0, 115.0, 850.0, 850.0};
+    Rectangle player = { 650, 280, 40, 40 };
+    
+    Camera2D camera = { 0 };
+    camera.target = (Vector2){ mainSqaure.x + 425, mainSqaure.y + 425 };
+    camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+   // camera.offset = (Vector2){ 425.0f, 425.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
-    while (!WindowShouldClose())
+    SetTargetFPS(144);               // Set our game to run at 144 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        // Update
+        //----------------------------------------------------------------------------------
+        //float deltaTime = GetFrameTime();
+        
+        //left and right collisions
+        if (player.x <= mainSqaure.x) player.x = mainSqaure.x;
+        else if (player.x + player.width >= mainSqaure.x + mainSqaure.width) player.x = (mainSqaure.x + mainSqaure.width)- player.width;
+
+        ///up and down collisions
+        if (player.y <= mainSqaure.y) player.y = mainSqaure.y;
+        else if (player.y + player.height >= mainSqaure.y + mainSqaure.height) player.y = (mainSqaure.y + mainSqaure.height) - player.height;
+        
+        
+        camera.target = (Vector2){ mainSqaure.x + 425, mainSqaure.y + 425};
+        if (IsKeyDown(KEY_A)) camera.rotation--;
+        else if (IsKeyDown(KEY_S)) camera.rotation++;
+        
+        if (camera.rotation > 180) camera.rotation = 180;
+        else if (camera.rotation < -180) camera.rotation = -180;
+        
+        if (IsKeyDown(KEY_RIGHT)) player.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) player.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) player.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) player.y += 2.0f;
+        
+        /*
+        switch((int)camera.rotation){
+        case -180:
+        player.y -= 5;
+        case -90:
+        player.x -= 5;
+        case 0:
+        player.y -= 5;
+        case 90:
+        player.x += 5;
+        case 180:
+        player.y -= 5;
+        }
+        */
+        
+        
+        
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
-
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
-
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+            ClearBackground(BLACK);
+            
+            BeginMode2D(camera);
+            
+            DrawRectangleRec(mainSqaure, DARKGRAY); 
+            
+            DrawRectangleRec(player, RED);
+            
+            EndMode2D();
 
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    CloseWindow();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
